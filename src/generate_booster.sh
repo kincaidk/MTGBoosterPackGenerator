2b6ad41cccc0c9449d3dd4fe834555a5e0b5859b
Kellen Kincaid <kellenceo@gmail.com>
2023-10-14 15:49:32 -0400

    Added the ability to generate multiple packs at a time. Also added the Brother's War booster composition

--- src/generate_booster.sh
+++ src/generate_booster.sh
@@ -4,15 +4,20 @@ _jq() {
     echo "${1}" | base64 --decode --ignore-garbage | jq -r "${2}"
 }
 
-setCode=$1
+# Arguments.
+setCode=$1              # 3-letter set code.
+numberOfBoosters=$2     # The number of booster packs to generate.
+
+# Path variables.
 setDataDirectory="set_data/${setCode}_set_data"
 boosterPackDirectory="booster_packs"
 boosterFile="${boosterPackDirectory}/${setCode}_booster.txt"
+boosterCompositionFilePath="src/booster_composition.json"
 
 # Generate set, if necessary.
 if [ ! -d "$setDataDirectory" ]
 then
-    bash "generate_set_data.sh" "${setCode}" 
+    bash "src/generate_set_data.sh" "${setCode}" 
 fi
 
 # Create necessary directories and files.
@@ -22,37 +27,41 @@ touch "$boosterFile"
 echo "GENERATING BOOSTER: ${setCode}"
 
 # Determine booster composition.
-boosterComposition=$(jq -r ".[] | select(.setCode==\"${setCode}\") | .booster" "booster_composition.json")
+boosterComposition=$(jq -r ".[] | select(.setCode==\"${setCode}\") | .booster" "$boosterCompositionFilePath")
 if [ -z "$boosterComposition" ]
 then
-    boosterComposition=$(jq -r ".[] | select(.setCode==\"default\") | .booster" "booster_composition.json")
+    boosterComposition=$(jq -r ".[] | select(.setCode==\"default\") | .booster" "$boosterCompositionFilePath")
 fi
 
-# Generate booster.
-for row in $(echo "${boosterComposition}" | jq -r '.[] | @base64'); do
-    rarity=$(_jq "${row}" '.rarity')
+# Generate booster(s).
+for ((i=0; i<$numberOfBoosters; i++)); do
+    for row in $(echo "${boosterComposition}" | jq -r '.[] | @base64'); do
+        rarity=$(_jq "${row}" '.rarity')
 
-    case "$rarity" in
-        "common"|"uncommon")
-            card=$(sed '/^\s*$/d' "${setDataDirectory}/${rarity}_${setCode}_cards.txt" | shuf -n 1)
-            echo "$card" >> "$boosterFile"
-            ;;
-        "rare")
-            # Rares have a 1/8 chance of being mythics.
-            randomNumber=$(shuf -i 1-8 -n 1)
-            if [ "$randomNumber" == "1" ]
-            then
-                rarity="mythic"
-            fi
+        case "$rarity" in
+            "common"|"uncommon")
+                card=$(sed '/^\s*$/d' "${setDataDirectory}/${rarity}_${setCode}_cards.txt" | shuf -n 1)
+                echo "$card" >> "$boosterFile"
+                ;;
+            "rare")
+                # Rares have a 1/8 chance of being mythics.
+                randomNumber=$(shuf -i 1-8 -n 1)
+                if [ "$randomNumber" == "1" ]
+                then
+                    rarity="mythic"
+                fi
 
-            card=$(sed '/^\s*$/d' "${setDataDirectory}/${rarity}_${setCode}_cards.txt" | shuf -n 1)
-            echo "$card" >> "$boosterFile"
-            ;;
-        *)
-            decodedRow=$(echo "$row" | base64 --decode --ignore-garbage)
-            urlSlugToGetCard=$(echo "$decodedRow" | jq '. | to_entries[] | "&\(.key)=\(.value)&pageSize=100"' | tr -d '"') # TODO: This will fail to get every card if there are more than 100.
-            specialCard=$(curl -s "https://api.magicthegathering.io/v1/cards?set=${setCode}${urlSlugToGetCard}" | jq --raw-output '[.cards[] | select(.number | test("^A-") | not)] | map(.name) | unique | .[]' | shuf -n 1)
-            echo "$specialCard" >> "$boosterFile"
-            ;;
-    esac
+                card=$(sed '/^\s*$/d' "${setDataDirectory}/${rarity}_${setCode}_cards.txt" | shuf -n 1)
+                echo "$card" >> "$boosterFile"
+                ;;
+            *)
+                decodedRow=$(echo "$row" | base64 --decode --ignore-garbage)
+                urlSlugToGetCard=$(echo "$decodedRow" | jq '. | to_entries[] | "&\(.key)=\(.value)&pageSize=100"' | tr -d '"') # TODO: This will fail to get every card if there are more than 100.
+                specialCard=$(curl -s "https://api.magicthegathering.io/v1/cards?set=${setCode}${urlSlugToGetCard}" | jq --raw-output '[.cards[] | select(.number | test("^A-") | not)] | map(.name) | unique | .[]' | shuf -n 1)
+                echo "$specialCard" >> "$boosterFile"
+                ;;
+        esac
+    done
+
+    echo "" >> "$boosterFile" # Separaion between booster packs.
 done
